85504406313d677ac12cdb67f5e3509aad78f10a
alejandrarivero <105026046+alejandrarivero@users.noreply.github.com>
2022-05-07 09:49:45 -0400

    Indicador 5 - Java

--- Main.java
+++ Main.java
@@ -6,21 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double contsucursal;
+        double cantidadproducto;
+        int indiceproducto;
+        int indiceproductomayor;
         int indicesucursal;
-        int indicevendedor;
+        double montomayor;
         double montoporsucursal;
+        String nombreproductomayor;
         double precioproducto;
         int producto;
         String producto_matrix[];
         double producto_precio_matrix[];
+        double producto_sucursal_cantidad[][];
+        double producto_sucursal_monto[][];
         int sucursal;
         String sucursal_matrix[];
         double total;
         double unidades;
-        int vendedor;
+        double vendedor;
         String vendedor_matrix[];
-        double ventas[][];
         // Se declara matriz de datos de 5 vendedores.
         vendedor_matrix = new String[5];
         vendedor_matrix[0] = "Percy    ";
@@ -47,7 +51,9 @@ public class Main {
         producto_precio_matrix[2] = 25;
         producto_precio_matrix[3] = 6;
         // Arreglo donde se almacenará las ventas de vendedor por sucursal.
-        ventas = new double[5][4];
+        // Dimension VENTAS(5, 4)
+        producto_sucursal_monto = new double[4][4];
+        producto_sucursal_cantidad = new double[4][4];
         // Se indica el ingreso de datos del vendedor.
         System.out.println("Ingrese el codigo de vendedor [0=Percy; 1= Felipe; 2=Alejandra; 3=Jorge; 4=Juan; -1=Fin]");
         vendedor = sc.nextInt();
@@ -62,37 +68,45 @@ public class Main {
             montoporsucursal = 0;
             while (producto!=-1) {
                 System.out.println("Ingrese la cantidad vendida");
-                unidades = sc.nextDouble();
+                unidades = sc.nextInt();
                 // Recupera el precio del producto en el arreglo PRODUCTO_PRECIO_MATRIX.
                 precioproducto = producto_precio_matrix[producto];
                 // Calculamos el monto total de la venta del producto.
                 total = precioproducto*unidades;
                 // Sumar el monto total en la variable montoPorSucursal
-                montoporsucursal = montoporsucursal+total;
+                // montoPorSucursal = montoPorSucursal + total
+                // Grabamos el total del producto en la sucursal en curso.
+                producto_sucursal_monto[sucursal][producto] = producto_sucursal_monto[sucursal][producto]+total;
+                producto_sucursal_cantidad[sucursal][producto] = producto_sucursal_cantidad[sucursal][producto]+unidades;
                 System.out.println("Ingrese el codigo de producto [0=Pollo; 1=Carne; 2=Cerdo; 3=Pavo; -1=Fin]");
                 producto = sc.nextInt();
             }
             // Grabamos la venta del vendedor en la sucursal actual para posterior uso en el indicador.
-            ventas[vendedor][sucursal] = montoporsucursal;
+            // VENTAS(vendedor, sucursal) = montoPorSucursal
             // Se vuelve a ingresar el vendedor.
             System.out.println("Ingrese el codigo de vendedor [0=Percy; 1= Felipe; 2=Alejandra; 3=Jorge; 4=Juan; -1=Fin]");
-            vendedor = sc.nextInt();
+            vendedor = sc.nextDouble();
         }
-        System.out.println("Se termin el ingreso de datos");
+        System.out.println("Se termino el ingreso de datos");
         System.out.println("Generando indicador");
         for (indicesucursal=0;indicesucursal<=3;indicesucursal++) {
             System.out.println(sucursal_matrix[indicesucursal]+": ");
-            contsucursal = 0;
-            for (indicevendedor=0;indicevendedor<=4;indicevendedor++) {
-                if (ventas[indicevendedor][indicesucursal]==0) {
-                    System.out.println("    "+vendedor_matrix[indicevendedor]);
-                    contsucursal = contsucursal+1;
+            // contSucursal = 0
+            montomayor = 0;
+            cantidadproducto = 0;
+            nombreproductomayor = "";
+            indiceproductomayor = 0;
+            for (indiceproducto=0;indiceproducto<=3;indiceproducto++) {
+                if (producto_sucursal_monto[indicesucursal][indiceproducto]>montomayor) {
+                    montomayor = producto_sucursal_monto[indicesucursal][indiceproducto];
+                    cantidadproducto = producto_sucursal_cantidad[indicesucursal][indiceproducto];
+                    nombreproductomayor = producto_matrix[indiceproducto];
+                    indiceproductomayor = indiceproducto;
                 }
             }
-            if (contsucursal==0) {
-                System.out.println("    => Todos los vendedores han realizado ventas");
-            } else {
-                System.out.println("Hay "+contsucursal+" vendedores que no realizaron ventas en la sucursal");
+            if (montomayor>0) {
+                System.out.println("    El producto "+nombreproductomayor+" es el mas vendido en la sucursal con una cantidad de "+cantidadproducto+" al precio de S/. "+producto_precio_matrix[indiceproductomayor]);
+                System.out.println("    Se ha generado un total de "+montomayor);
             }
         }
     }
